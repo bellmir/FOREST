a4147ae155fa03e0b3d0885dd896ff2b54804fef
Dever-Bellmir <bellmir1127@gmail.com>
2024-08-28 20:29:40 +0900

    feat: 대시보드 퍼블리싱

--- FOREST_front/src/pages/Home/Dashboard.vue
+++ FOREST_front/src/pages/Home/Dashboard.vue
@@ -1,20 +1,260 @@
 <template>
-	<main class="Home">
-		<section class="sample">
-			<div class="sample_content">홈 페이지</div>
+	<main class="Dashboard">
+		<h1 class="g_pageTitle">대시보드</h1>
+		<section class="carbonFootprint">
+			<h2 class="g_boxTitle">기업 탄소배출 목표</h2>
+			<div class="companyGoal">
+				<div class="companyBi">
+					<img src="@/assets/image/sample/company_sample_nike.svg" alt="" />
+				</div>
+				<div class="goalDetail">
+					<table class="goalTable">
+						<tr>
+							<th></th>
+							<th>목표 배출량</th>
+							<th>현재 배출량</th>
+							<th>목표 달성률</th>
+						</tr>
+						<tr>
+							<th>연별</th>
+							<td>960,000kg</td>
+							<td>540,300kg</td>
+							<td class="success">80%</td>
+						</tr>
+						<tr>
+							<th>월별</th>
+							<td>80,000kg</td>
+							<td>80,200kg</td>
+							<td class="danger">102%</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+			<div class="emissionStatus">
+				<div class="chartBox">
+					<Chart type="doughnut" class="chart" :data="todayChartData" :options="todayChartOption" />
+					<p class="chart_label">오늘 배출량</p>
+				</div>
+				<div class="chartBox">
+					<Chart type="bar" class="chart yearChart" :data="monthlyChartData" :options="monthlyChartOption" />
+					<p class="chart_label">연간 배출량</p>
+				</div>
+			</div>
+		</section>
+		<section class="inventoryBrief">
+			<div class="inventoryBrief_box">
+				<div class="g_boxTitle">
+					<IncomingIcon />
+					<h2>입고</h2>
+				</div>
+				<div class="box_content">
+					<p>233</p>
+				</div>
+			</div>
+			<div class="inventoryBrief_box">
+				<div class="g_boxTitle">
+					<OutgoingIcon />
+					<h2>출고</h2>
+				</div>
+				<div class="box_content">
+					<p>233</p>
+				</div>
+			</div>
 		</section>
 	</main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+//chart.js
+import Chart from 'primevue/chart';
+// image
+import IncomingIcon from '@/assets/image/dashboard/incoming.svg?component';
+import OutgoingIcon from '@/assets/image/dashboard/outgoing.svg?component';
+
+const documentStyle = getComputedStyle(document.body);
+
+const todayChartData = {
+	labels: ['생산', '유통', '기타'],
+	datasets: [
+		{
+			data: [540, 325, 20],
+			backgroundColor: [
+				documentStyle.getPropertyValue('--p-emerald-500'),
+				documentStyle.getPropertyValue('--p-lime-500'),
+				documentStyle.getPropertyValue('--p-gray-400'),
+			],
+			hoverBackgroundColor: [
+				documentStyle.getPropertyValue('--p-emerald-400'),
+				documentStyle.getPropertyValue('--p-lime-400'),
+				documentStyle.getPropertyValue('--p-gray-300'),
+			],
+		},
+	],
+};
+const todayChartOption = {
+	plugins: {
+		legend: {
+			labels: {
+				// usePointStyle: true,
+			},
+		},
+	},
+};
+
+const monthlyChartData = {
+	labels: ['1월', '2월', '3월', '4월', '5월', '6월', '7월', '8월', '9월', '10월', '11월', '12월'],
+	datasets: [
+		{
+			type: 'bar',
+			label: '생산',
+			backgroundColor: documentStyle.getPropertyValue('--p-orange-500'),
+			data: [50, 25, 40, 48, 90, 76, 42, 55],
+		},
+		{
+			type: 'bar',
+			label: '유통',
+			backgroundColor: documentStyle.getPropertyValue('--p-yellow-500'),
+			data: [21, 84, 24, 75, 37, 65, 34, 30],
+		},
+		{
+			type: 'bar',
+			label: '기타',
+			backgroundColor: documentStyle.getPropertyValue('--p-gray-400'),
+			data: [11, 32, 24, 14, 3, 21, 12, 10],
+		},
+	],
+};
+const monthlyChartOption = {
+	maintainAspectRatio: false,
+	aspectRatio: 0.8,
+	plugins: {
+		tooltips: {
+			mode: 'index',
+			intersect: false,
+		},
+	},
+	scales: {
+		x: {
+			stacked: true,
+		},
+		y: {
+			stacked: true,
+		},
+	},
+};
+</script>
 
 <style scoped lang="scss">
-.Home {
+.Dashboard {
 	@include mixin_mainContainer; // mainContainer 적용
-	.sample {
-		@include mixin_section;
-		.sample_content {
-			@include mixin_inner; // inner 적용
+	.carbonFootprint {
+		@include mixin_box;
+		.companyGoal {
+			display: flex;
+			gap: var(--space-x-large);
+			@media screen and (max-width: 768px) {
+				flex-direction: column;
+			}
+			.companyBi {
+				flex-grow: 1;
+				width: 20rem;
+				height: 14rem;
+				padding: var(--space-large);
+				background-color: var(--color-base-brand);
+				border-radius: var(--border-radius-large);
+				overflow: hidden;
+
+				@media screen and (max-width: 768px) {
+					width: 100%;
+				}
+				img {
+					@include mixin_fullImg($object-fit: contain);
+				}
+			}
+			.goalDetail {
+				flex-grow: 1;
+				.goalTable {
+					border: 1px solid var(--color-base-mid);
+					width: 100%;
+					th,
+					td {
+						padding: var(--space-x-small) var(--space-x-small);
+						border: 1px solid var(--color-base-mid);
+						text-align: center;
+					}
+					th {
+						font-weight: var(--font-w-semi);
+						font-size: var(--font-s-mid);
+						background-color: var(--color-base-brand);
+					}
+					td {
+						&.success {
+							color: var(--color-success);
+						}
+						&.danger {
+							color: var(--color-danger);
+						}
+					}
+				}
+			}
+		}
+		.emissionStatus {
+			display: flex;
+			gap: var(--space-x-large);
+			margin-top: var(--space-2x-large);
+			@media screen and (max-width: 768px) {
+				flex-direction: column;
+			}
+			.chartBox {
+				@include mixin_flexCenter;
+				flex-direction: column;
+				width: calc((100% - var(--space-x-large)) / 2);
+				@media screen and (max-width: 768px) {
+					width: 100%;
+				}
+				.chart {
+					height: 45rem;
+					max-width: 45rem;
+					width: 100%;
+					@media screen and (max-width: 768px) {
+						height: auto;
+						aspect-ratio: 1;
+					}
+					&.yearChart {
+						max-width: 70rem;
+					}
+				}
+				.chart_label {
+					margin-top: var(--space-x-small);
+					text-align: center;
+					font-weight: var(--font-w-mid);
+				}
+			}
+		}
+	}
+	.inventoryBrief {
+		display: grid;
+		grid-template-columns: 1fr 1fr;
+		gap: 0 var(--space-x-large);
+		@media screen and (max-width: 768px) {
+			grid-template-columns: 1fr;
+		}
+		.inventoryBrief_box {
+			@include mixin_box(true);
+			.g_boxTitle {
+				display: flex;
+				align-items: center;
+				gap: var(--space-mid);
+				svg {
+					width: 1em;
+					height: 1em;
+					fill: currentColor;
+				}
+			}
+			.box_content {
+				font-size: var(--font-s-x-large);
+				font-weight: var(--font-w-mid);
+			}
 		}
 	}
 }
